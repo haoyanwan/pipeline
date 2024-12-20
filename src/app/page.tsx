192326d5ea765db32eda9019cb05867ea580908e
Haoyan Wan <haoyanlgd@gmail.com>
2024-12-19 19:46:14 -0800

    first

---
+++ src/app/page.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import Image from "next/image";
+import styles from "../../styles/page.module.css";
+import { BackgroundVariant } from '@xyflow/react';
+import { useCallback } from 'react';
+import {
+  ReactFlow,
+  useNodesState,
+  useEdgesState,
+  useEdges,
+  addEdge,
+  Connection,
+  Edge,
+  MiniMap,
+  Controls,
+  Background,
+} from '@xyflow/react';
+import '@xyflow/react/dist/style.css';
+
+
+const initialNodes = [
+  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+
+
+export default function Home() {
+
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes); 
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect = useCallback(
+    (params: Connection) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges],
+  )
+
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <ReactFlow 
+        nodes={nodes} 
+        edges={edges} 
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+      >
+        <MiniMap />
+        <Controls />
+        <Background variant={BackgroundVariant.Lines} gap={12} size={1}/>
+      </ReactFlow>
+    </div>
+  );
+}
